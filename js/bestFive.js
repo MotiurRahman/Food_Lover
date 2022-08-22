79762ec6ddb06350824a6a96fb37f8da7a3b1d9d
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-08-22 15:14:21 +0600

    add disabled feature

--- js/bestFive.js
+++ js/bestFive.js
@@ -2,18 +2,23 @@
 // Select Five Food
 let foodBucket = [];
 document.getElementById('allFood').addEventListener('click', function (e) {
-    foodBucket.push(e.target.dataset.food);
-    let foodList = document.getElementById('foodList');
-    let foodItem = document.createElement('li');
-    foodItem.classList = "list-group-item";
+    let selectItem = e.target;
 
-    if (foodBucket.length <= 5) {
-        for (var i in foodBucket) {
-            foodItem.innerText = foodBucket[i];
-            foodList.appendChild(foodItem);
+    if (selectItem.innerText == "Select") {
+        foodBucket.push(e.target.dataset.food);
+        let foodList = document.getElementById('foodList');
+        let foodItem = document.createElement('li');
+        foodItem.classList = "list-group-item";
+
+        if (foodBucket.length <= 5) {
+            for (var i in foodBucket) {
+                foodItem.innerText = foodBucket[i];
+                foodList.appendChild(foodItem);
+            }
+            selectItem.disabled = true;
+        } else {
+            alert("Can not add more than 5 food.");
         }
-    } else {
-        alert("Can not add more than 5 food.");
     }
 });
 

18cd5c8d2c6c952e61ef914628b9a5049336176b
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-08-23 10:24:34 +0600

    Fix some validation

--- js/budget.js
+++ js/budget.js
@@ -1,8 +1,11 @@
+
+//Get input field value
 function getInputFieldValue(elementField) {
     let inputFieldValue = parseInt(document.getElementById(elementField).value);
     return inputFieldValue;
 }
 
+//set text value
 function setTextValue(elementField, textValue) {
     document.getElementById(elementField).innerText = textValue;
 }
@@ -11,8 +14,8 @@ function setTextValue(elementField, textValue) {
 // Calculate Food Cost
 document.getElementById('foodExpensesCalculation').addEventListener('click', function () {
     let perFoodBudgetField = getInputFieldValue("perFoodBudgetField");
-    if (isNaN(perFoodBudgetField)) {
-        alert("Pleaes enter a valid number")
+    if (isNaN(perFoodBudgetField) || perFoodBudgetField < 0) {
+        alert("Pleaes enter a valid cost")
         document.getElementById('perFoodBudgetField').value = "";
         return false;
     }
@@ -34,13 +37,13 @@ document.getElementById('calculateTotal').addEventListener('click', function () 
     let shippingCost = getInputFieldValue('shippingCost');
     let cookingCost = getInputFieldValue('cookingCost');
     let foodExpense = document.getElementById('foodExpense').innerText;
-    if (isNaN(shippingCost)) {
+    if (isNaN(shippingCost) || shippingCost < 0) {
         alert("Pleaes enter valid shipping cost")
         document.getElementById('shippingCost').value = "";
         return false;
     }
 
-    if (isNaN(cookingCost)) {
+    if (isNaN(cookingCost) || cookingCost < 0) {
         alert("Pleaes enter valid cooking cost")
         document.getElementById('cookingCost').value = "";
         return false;
